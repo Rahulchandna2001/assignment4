31da05d52c26fecf8d4a99003b2a8b97356d012b
Rahulchandna2001 <95170585+Rahulchandna2001@users.noreply.github.com>
2024-11-22 20:45:41 -0500

    adding sql file and scripting files

---
+++ create_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Students (
+    StudentID INT PRIMARY KEY,
+    Name NVARCHAR(50),
+    Age INT
+);
+
---
+++ update_table.sql
@@ -0,0 +1,4 @@
+-- Script to add a new column to the Students table
+ALTER TABLE Students
+ADD Email NVARCHAR(100);
+
